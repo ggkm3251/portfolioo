9fa26ffae9396495f54b0e51fc78ddda8a02e937
glenn <glennkamau2@gmail.com>
2025-04-23 11:27:35 +0300

    cv

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -43,7 +43,7 @@ export const About: React.FC = () => {
             
             <div className="flex flex-wrap gap-4">
               <a 
-                href="#"
+                href="/portfolioo/Glenn_Mwangi_CV.pdf"
                 className="btn btn-primary px-6 py-2.5 flex items-center gap-2"
               >
                 <Download className="h-4 w-4" />
